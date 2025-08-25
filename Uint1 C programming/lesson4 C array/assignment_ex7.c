c5d92451a03a45c153c6617271a5e59daf7b5caa
ahmed-mego <ahmedmego2555@gmail.com>
2025-08-25 18:19:59 +0200

    assignment 7 string

---
+++ Uint1 C programming/lesson4 C array/assignment_ex7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    char str[50];
+    int i = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    while(str[i] != '\0') {
+        i++;
+    }
+
+    printf("lenght of string: %d", i);
+}
