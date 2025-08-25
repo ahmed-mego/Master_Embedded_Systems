a62dbcc0a99c99f5809a83b2ac8c688c7ae0dad7
ahmed-mego <ahmedmego2555@gmail.com>
2025-08-25 12:57:17 +0200

    assignment 3

--- Uint1 C programming/lesson4 C array/assignment_ex3.c
+++ Uint1 C programming/lesson4 C array/assignment_ex3.c
@@ -1,16 +1,47 @@
 #include <stdio.h>
 
 int main() {
-    float matrixA[2][2];
-    int i, j;
+    int i, j, r, c;
 
-    // Input the matrix 
-    printf("Enter elements of matrix:\n");
-    for (i = 0; i < 2; i++) {
-        for (j = 0; j < 2; j++) {
+    // Input the matrix dimensions
+    printf("Enter rows and columns of matrix: ");
+    scanf("%d %d", &r, &c);
+
+    int matrix[r][c];
+    int matrixT[c][r];
+
+    // Input the matrix
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
             printf("matrixA[%d][%d] = ", i+1, j+1);
-            scanf("%f", &matrixA[i][j]);
+            scanf("%d", &matrix[i][j]);
         }
     }
 
+    // Calculate transpose 
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            matrixT[j][i] = matrix[i][j];
+        }
+    }
+
+    // Print original matrix
+    printf("\nOriginal Matrix:\n");
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Print transpose
+    printf("\nTranspose Matrix:\n");
+    for (i = 0; i < c; i++) {
+        for (j = 0; j < r; j++) {
+            printf("%d\t", matrixT[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
